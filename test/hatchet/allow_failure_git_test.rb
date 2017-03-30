a2231a939e8df123a60bed9e9948a5acb1e92427
Benoit Tigeot <btigeot@octo.com>
2017-03-30 21:00:31 +0200

    Fix failing test when the folder exist but fails to deploy

--- test/hatchet/allow_failure_git_test.rb
+++ test/hatchet/allow_failure_git_test.rb
@@ -10,8 +10,12 @@ class AllowFailureGitTest < Test::Unit::TestCase
   end
 
   def test_failure_with_no_flag
+    FileUtils.mkdir('no_lockfile') unless File.exists?('no_lockfile')
+
     assert_raise(Hatchet::App::FailedDeploy) do
       Hatchet::GitApp.new("no_lockfile").deploy
     end
+
+    FileUtils.rm_r('no_lockfile')
   end
 end
